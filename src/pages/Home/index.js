99f1e79c95bce6bc1c57493998563e48cd0f3040
Fabiana Pereira <fabianaduarte980@gmail.com>
2024-12-18 00:59:37 -0300

    wip: add home page structure

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,11 @@
+import { Header } from '../../components'
 import './style.css'
 
 export const Home = () => {
-  return <div>tela inicial</div>
+  return (
+    <>
+      <Header />
+      <main className="max-w-5xl mx-auto p-10 flex flex-col items-center"></main>
+    </>
+  )
 }
