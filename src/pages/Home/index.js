ebe9db66c38884ddba57e40c4f2538e154632a9a
Dagson Gabriel <dagson.g.01@gmail.com>
2024-12-19 03:27:01 -0300

    feat: implement search functionality with loading state in home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,25 @@
-import { Header } from '../../components'
+import { useState } from 'react'
+import { Header, SearchField, Image, Loading } from '../../components'
+import { searchHandler } from '../../utils/searchHandler'
 import './style.css'
 
 export const Home = () => {
+  const [catTags, setCatTags] = useState([])
+  const [loading, setLoading] = useState(false)
+
   return (
     <>
       <Header />
-      <main className="max-w-5xl mx-auto p-10 flex flex-col items-center"></main>
+      <SearchField placeholder="Buscar gatinho" onChange={e => { searchHandler(e, setCatTags, setLoading) }} />
+      <main className="max-w-5xl mx-auto p-10 flex flex-col items-center">
+        <div className="flex justify-center h-[70vh] items-center w-full">
+          {loading ? (
+            <Loading />
+          ) : (
+            <Image tags={catTags} height="h-full" width="md:w-2/3" />
+          )}
+        </div>
+      </main>
     </>
   )
 }
