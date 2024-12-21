e706d2a27866f11ec4aed11c1601145209f7db55
Fabiana Pereira <fabianaduarte980@gmail.com>
2024-12-20 22:07:07 -0300

    refactor: delete ref

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,8 +1,8 @@
 import { findCatById } from '../utils/url'
 
-export const Image = ({ id = null, height = 'h-64', width = 'w-full', ref = null }) => {
+export const Image = ({ id = null, height = 'h-64', width = 'w-full' }) => {
   let url = ''
   if (id && id.length > 0) url = findCatById(id)
 
-  return <img src={url} alt="Imagem de gato" className={`rounded-2xl object-cover ${height} ${width}`} ref={ref} />
+  return <img src={url} alt="Imagem de gato" className={`rounded-2xl object-cover ${height} ${width}`} />
 }
